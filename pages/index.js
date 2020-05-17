8d051f7be14f9391608db94c6f1089b216626653
Luis Güette <guetteluis@gmail.com>
2020-05-17 01:17:00 -0400

    add blog section

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function App() {
 
 			<div className="relative mt-20 pl-4 pr-8 text-white max-w-4xl mx-auto sm:mt-32 lg:max-w-6xl">
 				<div
-					className="absolute z-0 bg-gray-800 opacity-50 mt-40 -mr-40 right-0"
+					className="absolute z-0 bg-gray-800 opacity-50 transform translate-y-40 translate-x-40 right-0"
 					style={{ height: '1200px', width: '600px' }}
 				/>
 				<div className="absolute inset-0 z-10 ml-10 mr-4 mt-4 -mb-4 border-2 border-white hover:bg-gray-700 focus:bg-gray-700 transition duration-500 ease-in-out" />
@@ -92,12 +92,114 @@ export default function App() {
 				</div>
 			</div>
 
-			<div className="relative text-white mt-20 sm:mt-32">
-				<h2 className="text-center font-light text-3xl leading-relaxed lg:text-5xl xl:text-4xl">
+			<div className="relative text-white mt-20 max-w-6xl mx-auto sm:mt-32">
+				<div
+					className="absolute z-0 left-0 bg-gray-800 opacity-50 transform translate-y-64"
+					style={{ height: '1000px', width: '400px' }}
+				/>
+
+				<h2 className="relative text-center font-light text-3xl leading-relaxed lg:text-5xl">
 					Check my
 					<br />
 					<span className="uppercase font-bold tracking-10">
 						Latest posts
+					</span>
+				</h2>
+
+				<div className="relative mt-10 space-y-8 flex flex-wrap justify-center sm:space-y-0">
+					<div
+						className="hidden absolute inset-x-0 h-96 max-w-lg mx-auto bg-gray-700 opacity-75 shadow-lg sm:block lg:h-72 lg:max-w-3xl"
+						style={{ top: '60px' }}
+					/>
+					<a href="#" className="relative w-full sm:w-auto sm:p-4">
+						<div className="relative h-72 w-72 mx-auto transform hover:scale-105 hover:shadow-xl focus:scale-105 focus:shadow-xl transition duration-300 ease-in-out">
+							<img
+								className="absolute inset-0"
+								src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=60"
+								alt=""
+							/>
+							<div className="absolute inset-0 bg-gray-900 opacity-50" />
+
+							<div className="absolute bottom-0 p-4">
+								<p className="text-sm">Sep 27, 2017</p>
+								<p className="font-bold">
+									How to Configure an API with ExpressJS
+								</p>
+							</div>
+						</div>
+					</a>
+
+					<a href="#" className="relative w-full sm:w-auto sm:p-4">
+						<div className="relative h-72 w-72 mx-auto transform hover:scale-105 hover:shadow-xl focus:scale-105 focus:shadow-xl transition duration-300 ease-in-out">
+							<img
+								className="absolute inset-0"
+								src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=60"
+								alt=""
+							/>
+							<div className="absolute inset-0 bg-gray-900 opacity-50" />
+
+							<div className="absolute bottom-0 p-4">
+								<p className="text-sm">Sep 27, 2017</p>
+								<p className="font-bold">
+									How to Configure an API with ExpressJS
+								</p>
+							</div>
+						</div>
+					</a>
+
+					<a href="#" className="relative w-full sm:w-auto sm:p-4">
+						<div className="relative h-72 w-72 mx-auto transform hover:scale-105 hover:shadow-xl focus:scale-105 focus:shadow-xl transition duration-300 ease-in-out">
+							<img
+								className="absolute inset-0"
+								src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=60"
+								alt=""
+							/>
+							<div className="absolute inset-0 bg-gray-900 opacity-50" />
+
+							<div className="absolute bottom-0 p-4">
+								<p className="text-sm">Sep 27, 2017</p>
+								<p className="font-bold">
+									How to Configure an API with ExpressJS
+								</p>
+							</div>
+						</div>
+					</a>
+
+					<div className="relative w-full pt-4 px-4 text-center md:pt-10 lg:pt-12">
+						<p className="font-light">Learn more about</p>
+
+						<div className="flex flex-wrap justify-center space-x-4">
+							<a
+								href="#"
+								className="relative nav-link py-2 text-white font-medium uppercase tracking-24"
+							>
+								Mobility
+							</a>
+
+							<a
+								href="#"
+								className="relative nav-link py-2 text-white font-medium uppercase tracking-24"
+							>
+								Web development
+							</a>
+
+							<a
+								href="#"
+								className="relative nav-link py-2 text-white font-medium uppercase tracking-24"
+							>
+								Design
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="relative text-white mt-20 sm:mt-32">
+				<h2 className="text-center font-light text-3xl leading-relaxed lg:text-5xl">
+					Or see
+					<br />
+					<span className="uppercase font-bold tracking-10">
+						What I'm working on
 					</span>
 				</h2>
 			</div>
